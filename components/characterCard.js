5baab1d4027296ed91ca1a8c02fdb4db145a1f5a
lmachens <leon@machens.koeln>
2021-10-20 16:51:57 +0200

    Fetch last known location

--- components/characterCard.js
+++ components/characterCard.js
@@ -7,8 +7,12 @@ export default function createCharacterCard({
   status,
   race,
   lastKnownLocation,
-  firstSeenIn,
+  episode,
 }) {
+  const firstEpisode = episode[0];
+
+  const firstEpisodeElement = createElement('p', { textContent: 'Loading...' });
+
   const characterCard = createElement(
     'article',
     {
@@ -19,11 +23,19 @@ export default function createCharacterCard({
       createElement('h2', { textContent: name }),
       createElement('p', { textContent: `${status} - ${race}` }),
       createElement('h3', { textContent: 'Last known location:' }),
-      createElement('p', { textContent: lastKnownLocation }),
+      createElement('p', {
+        textContent: lastKnownLocation,
+      }),
       createElement('h3', { textContent: 'First seen in:' }),
-      createElement('p', { textContent: firstSeenIn }),
+      firstEpisodeElement,
     ]
   );
+
+  fetch(firstEpisode)
+    .then((response) => response.json())
+    .then((body) => {
+      firstEpisodeElement.textContent = body.name;
+    });
 
   return characterCard;
 }
--- main.js
+++ main.js
@@ -20,7 +20,7 @@ async function renderApp() {
   const response = await fetch('https://rickandmortyapi.com/api/character');
   const body = await response.json();
   const characters = body.results;
-
+  console.log(characters);
   const characterCards = characters.map((character) =>
     createCharacterCard(character)
   );
